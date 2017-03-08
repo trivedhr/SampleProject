c616890d49556ceaea9557c2c466a193eb224109
trivedh reddy <trivedh.reddy01@outlook.com>
2017-03-08 12:06:15 -0800

    program.cs added comment

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -11,6 +11,7 @@ namespace SampleProject
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// //commnet added
         [STAThread]
         static void Main()
         {
